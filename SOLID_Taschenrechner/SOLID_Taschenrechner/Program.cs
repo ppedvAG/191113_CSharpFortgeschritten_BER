a83b8dd9a7f76513cfa77474eb2d60f7b6399063
Michael Zöhling <michael.zoehling@outlook.com>
2019-11-14 10:02:27 +0100

    Schritt 1: Einzelne Aufgaben definieren und zerlegen

--- SOLID_Taschenrechner/SOLID_Taschenrechner/Program.cs
+++ SOLID_Taschenrechner/SOLID_Taschenrechner/Program.cs
@@ -8,28 +8,68 @@ namespace SOLID_Taschenrechner
 {
     class Program
     {
+        // Bootstrapping -> Initialisieren der Logik
         static void Main(string[] args)
         {
+            new KonsolenUI().Start();
+        }
+    }
+    public struct Formel
+    {
+        public int Operand1 { get; set; }
+        public int Operand2 { get; set; }
+        public string Operator { get; set; }
+    }
+
+    public class StringSplitParser
+    {
+        public Formel Parse(string input)
+        {
+            string[] parts = input.Split();
+            Formel output = new Formel();
+            output.Operand1 = Convert.ToInt32(parts[0]);
+            output.Operator = parts[1];
+            output.Operand2 = Convert.ToInt32(parts[2]);
+
+            return output;
+        }
+    }
+
+    public class SimplerRechner
+    {
+        public int Berechne(Formel input)
+        {
+            if (input.Operator == "+")                  // entscheidung
+                return input.Operand1 + input.Operand2; // berechnung
+            else if (input.Operator == "-")
+                return input.Operand1 - input.Operand2;
+            // .....
+
+            throw new InvalidOperationException($"Operator {input.Operator} ist unbekannt");
+        }
+    }
+
+    public class KonsolenUI
+    {
+        public void Start()
+        {
+            // I/O
             Console.WriteLine("Bitte geben Sie die Formel ein:");
             string eingabe = Console.ReadLine(); // "2 + 2"
 
-            string[] parts = eingabe.Split();
+            // Parsen
+            var parser = new StringSplitParser();
+            var formel = parser.Parse(eingabe);
 
-            int zahl1 = Convert.ToInt32(parts[0]);
-            string op = parts[1];
-            int zahl2 = Convert.ToInt32(parts[2]);
+            // Rechnen
+            var rechner = new SimplerRechner();
+            var ergebnis = rechner.Berechne(formel);
 
-            int ergebnis = 0;
-            if (op == "+")
-                ergebnis = zahl1 + zahl2;
-            else if (op == "-")
-                ergebnis = zahl1 - zahl2;
-            // .....
-
+            //I/O
             Console.WriteLine($"Das Ergebnis ist {ergebnis}");
-
             Console.WriteLine("---ENDE---");
             Console.ReadKey();
         }
     }
+
 }
