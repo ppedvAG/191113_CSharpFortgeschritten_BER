16c145d7e7809f1fdb56a2aee08c3a93d15e4e3b
Michael Zöhling <michael.zoehling@outlook.com>
2019-11-14 14:39:07 +0100

    Normales Repository erstellt

--- HalloAspektorientiertesProgrammieren/HalloAspektorientiertesProgrammieren/Program.cs
+++ HalloAspektorientiertesProgrammieren/HalloAspektorientiertesProgrammieren/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Data;
+using Domain;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -11,7 +13,23 @@ namespace HalloAspektorientiertesProgrammieren
         static void Main(string[] args)
         {
 
+            EFRepository repo = new EFRepository(new EFContext());
 
+
+            Person p1 = new Person();
+            p1.Vorname = "Tom";
+            p1.Nachname = "Ate";
+            p1.Alter = 20;
+            p1.Kontostand = 200m;
+
+            // Irgendeine Logik
+            repo.Add(p1);
+            var loaded = repo.GetByID<Person>(1);
+            var all = repo.GetAll<Person>();
+            p1.Nachname = "Atinger";
+            repo.Update(p1);
+            repo.Delete(p1);
+            repo.Save();
 
             Console.WriteLine("---ENDE---");
             Console.ReadKey();
