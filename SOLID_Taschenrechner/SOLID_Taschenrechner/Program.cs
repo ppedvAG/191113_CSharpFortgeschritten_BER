3ff1302697d6ae1e28582b5bba4bf05d624dfd7b
Michael Zöhling <michael.zoehling@outlook.com>
2019-11-14 14:05:29 +0100

    FreeFeatures und PaidFeatures aus dem /bin/debug/Plugins - Ordner zur Laufzeit laden und Anwenden

--- SOLID_Taschenrechner/SOLID_Taschenrechner/Program.cs
+++ SOLID_Taschenrechner/SOLID_Taschenrechner/Program.cs
@@ -1,7 +1,9 @@
-﻿using FreeFeatures;
-using PaidFeatures;
+﻿using Domain;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
@@ -14,8 +16,36 @@ namespace SOLID_Taschenrechner
         // Bootstrapping -> Initialisieren der Logik
         static void Main(string[] args)
         {
+            // 1) Alle Dateien laden:
+
+            List<Assembly> customLoadedAssemblies = new List<Assembly>();
+
+            if (!Directory.Exists(".\\Plugins"))
+                Directory.CreateDirectory(".\\Plugins"); // Damit keine Exception kommt !
+
+            foreach (var file in Directory.GetFiles(".\\Plugins"))
+            {
+                if (Path.GetExtension(file) == ".dll" ||
+                    Path.GetExtension(file) == ".exe")
+                       customLoadedAssemblies.Add(Assembly.LoadFrom(file));
+            }
+
+            // Alle aktuell geladenen Assemblies:
+            // var all = AppDomain.CurrentDomain.GetAssemblies();
+
+            // Ziel: Alles, was die Schnittstelle "IRechenart" implementiert, mit Activator instanzieren und dem Modularrechner übergeben
+
+            IRechenart[] loadedRechenarten = customLoadedAssemblies.SelectMany(x => x.GetTypes())
+                                                                   .Where(x => typeof(IRechenart).IsAssignableFrom(x) &&
+                                                                               x.IsAbstract == false && // gilt für static UND abstract
+                                                                               x.IsInterface == false)
+                                                                   .Select(x => (IRechenart)Activator.CreateInstance(x))
+                                                                   .ToArray();
+
+
+            var rechner = new ModularRechner(loadedRechenarten);
             var parser = new RegexParser();
-            var rechner = new ModularRechner(new Addition(), new Subtraktion(), new Division());
+            //var rechner = new ModularRechner(new Addition(), new Subtraktion(), new Division());
             new KonsolenUI(parser, rechner).Start();
         }
     }
