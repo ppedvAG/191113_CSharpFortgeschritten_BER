defe017cfa055c7681172ed33242a797aac590f6
Michael Zöhling <michael.zoehling@outlook.com>
2019-11-14 09:46:18 +0100

    Logik ohne SOLID

--- SOLID_Taschenrechner/SOLID_Taschenrechner/Program.cs
+++ SOLID_Taschenrechner/SOLID_Taschenrechner/Program.cs
@@ -10,8 +10,26 @@ namespace SOLID_Taschenrechner
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Bitte geben Sie die Formel ein:");
+            string eingabe = Console.ReadLine(); // "2 + 2"
 
+            string[] parts = eingabe.Split();
 
+            int zahl1 = Convert.ToInt32(parts[0]);
+            string op = parts[1];
+            int zahl2 = Convert.ToInt32(parts[2]);
+
+            int ergebnis = 0;
+            if (op == "+")
+                ergebnis = zahl1 + zahl2;
+            else if (op == "-")
+                ergebnis = zahl1 - zahl2;
+            // .....
+
+            Console.WriteLine($"Das Ergebnis ist {ergebnis}");
+
+            Console.WriteLine("---ENDE---");
+            Console.ReadKey();
         }
     }
 }
