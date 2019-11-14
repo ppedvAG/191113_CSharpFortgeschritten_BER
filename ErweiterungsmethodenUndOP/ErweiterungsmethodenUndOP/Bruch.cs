61421527903dd0620573bf3ba2b1108a4ece5c84
Michael Zöhling <michael.zoehling@outlook.com>
2019-11-14 16:30:53 +0100

    Operatorüberladung

---
+++ ErweiterungsmethodenUndOP/ErweiterungsmethodenUndOP/Bruch.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ErweiterungsmethodenUndOP
+{
+    public class Bruch
+    {
+        public Bruch(int zähler, int nenner)
+        {
+            Zähler = zähler;
+            Nenner = nenner;
+        }
+
+        public int Zähler { get; set; }
+        public int Nenner { get; set; }
+
+        // Operatorüberladung:
+
+        // Arithmetische Operatoren:
+        // +,-,*,/,%, .... +=, -= 
+        // Bit-Operatoren
+        // &,|,^, <<,>>, 
+        // Vergleichsoperatoren:
+        // <,>,<=,>=, == ,!=
+        // ----> paarweise überladen !!!
+
+        public static Bruch operator*(Bruch left, Bruch right)
+        {
+            return new Bruch(left.Zähler * right.Zähler, left.Nenner * right.Nenner);
+        }
+
+        public static Bruch operator *(Bruch left, int right)
+        {
+            return new Bruch(left.Zähler * right, left.Nenner);
+        }
+
+
+        public static bool operator == (Bruch left, Bruch right)
+        {
+            return true;
+        }
+        public static bool operator !=(Bruch left, Bruch right)
+        {
+            return false;
+        }
+
+
+        public override string ToString()
+        {
+            return $"{Zähler}/{Nenner}";
+        }
+    }
+}
--- ErweiterungsmethodenUndOP/ErweiterungsmethodenUndOP/Program.cs
+++ ErweiterungsmethodenUndOP/ErweiterungsmethodenUndOP/Program.cs
@@ -14,6 +14,17 @@ namespace ErweiterungsmethodenUndOP
             Erweiterungsmethoden.Verdoppeln(zahl1);
             zahl1.Verdoppeln();
 
+            Bruch b1 = new Bruch(1, 2);
+            Bruch b2 = new Bruch(1, 4);
+
+            Bruch erg = b1 * b2;
+            Bruch erg2 = b1 * 3;
+
+            b1 *= 12;
+
+            Console.WriteLine($"Das Ergebnis ist: {erg}");
+            Console.WriteLine($"Das Ergebnis ist: {erg2}");
+
             Console.WriteLine("---ENDE---");
             Console.ReadKey();
         }
